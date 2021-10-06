6d8e66edbb291ce068e173eb0d0d620154cb96c1
Vlad Verba <vv2298@columbia.edu>
2021-10-05 20:01:23 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+function validateFormInput() {
+  var errorBox = document.getElementsByClassName("error_message")[0];
+
+  firstName = checkFirstName();
+  if (firstName == true) {
+    emailValidate = validateEmail();
+    if (emailValidate == true) {
+      errorBox.textContent = "Great Success!";
+      errorBox.style.color = "green";
+    }
+  }
+}
+
+function checkFirstName() {
+  var nameInput = document.getElementById("first_name");
+  var errorBox = document.getElementsByClassName("error_message")[0];
+
+  if (nameInput.value.length < 2) {
+    errorBox.textContent = "Please enter a valid first name.";
+    errorBox.style.color = "red";
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function validateEmail() {
+  var emailInput = document.getElementById("user_email");
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  var errorBox = document.getElementsByClassName("error_message")[0];
+
+  if (emailInput.value.match(mailformat)) {
+    return true;
+  } else {
+    errorBox.textContent = "Please enter a valid email.";
+    errorBox.style.color = "red";
+
+    return false;
+  }
+}
